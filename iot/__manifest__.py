5b658ee76c440e06070adfd0412d0db52724a5ca
Enric Tobella <etobella@creublanca.es>
2018-07-26 16:26:27 +0200

    [FIX] security

--- iot/__manifest__.py
+++ iot/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'IoT Base',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'category': 'IoT',
     'author': "Creu Blanca, "
               "Odoo Community Association (OCA)",
