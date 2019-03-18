6c5d915ba430eebd4d355eaefe87869a4708acc5
Enric Tobella <etobella@creublanca.es>
2019-03-18 11:24:59 +0100

    [ADD] iot_output

---
+++ iot_output_oca/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'IoT Output',
+    'version': '11.0.1.0.0',
+    'category': 'IoT',
+    'author': "Creu Blanca, "
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+    'summary': 'IoT allow multiple outputs',
+    'depends': [
+        'iot',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/iot_device_output_views.xml',
+        'views/iot_device_views.xml',
+    ],
+}
---
+++ iot_output_oca/models/__init__.py
@@ -0,0 +1,4 @@
+from . import iot_device
+from . import iot_device_output_action
+from . import iot_device_output
+from . import iot_system
