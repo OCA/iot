bcfb4d0828d5b096e5578bb9d4d7cbcf9396c0b7
Enric Tobella <etobella@creublanca.es>
2019-03-18 10:49:04 +0100

    [IMP] iot: views

--- iot_oca/__manifest__.py
+++ iot_oca/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'IoT Base',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.2',
     'category': 'IoT',
     'author': "Creu Blanca, "
               "Odoo Community Association (OCA)",
