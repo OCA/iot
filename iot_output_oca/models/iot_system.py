dc165150ad30a34eebbab611e35b927221d41958
Olga Marco <olga.marco@creublanca.es>
2021-06-08 17:54:10 +0200

    [IMP] iot_output_oca: black, isort, prettier

--- iot_output_oca/models/iot_system.py
+++ iot_output_oca/models/iot_system.py
@@ -4,10 +4,9 @@ from odoo import fields, models
 
 
 class IoTSystem(models.Model):
-    _inherit = 'iot.system'
+    _inherit = "iot.system"
 
-    output_ids = fields.One2many('iot.device.output', inverse_name='system_id')
-    applies_to = fields.Selection([
-        ('device', 'Device'),
-        ('output', 'Output'),
-    ], default='device', required=True)
+    output_ids = fields.One2many("iot.device.output", inverse_name="system_id")
+    applies_to = fields.Selection(
+        [("device", "Device"), ("output", "Output"),], default="device", required=True
+    )
