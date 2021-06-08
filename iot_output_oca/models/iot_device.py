dacae3c7265ed665942df4c581c805c034ea76aa
Olga Marco <olga.marco@creublanca.es>
2021-06-08 17:54:10 +0200

    [IMP] iot_output_oca: black, isort, prettier

--- iot_output_oca/models/iot_device.py
+++ iot_output_oca/models/iot_device.py
@@ -4,17 +4,14 @@ from odoo import api, fields, models
 
 
 class IoTDevice(models.Model):
-    _inherit = 'iot.device'
+    _inherit = "iot.device"
 
-    output_ids = fields.One2many(
-        'iot.device.output',
-        inverse_name='device_id'
-    )
-    output_count = fields.Integer(compute='_compute_output_count')
+    output_ids = fields.One2many("iot.device.output", inverse_name="device_id")
+    output_count = fields.Integer(compute="_compute_output_count")
     system_id = fields.Many2one(required=False)
 
     @api.multi
-    @api.depends('output_ids')
+    @api.depends("output_ids")
     def _compute_output_count(self):
         for record in self:
             record.output_count = len(record.output_ids)
@@ -22,16 +19,14 @@ class IoTDevice(models.Model):
     @api.multi
     def action_show_output(self):
         self.ensure_one()
-        action = self.env.ref(
-            'iot_output.iot_device_output_action')
+        action = self.env.ref("iot_output.iot_device_output_action")
         result = action.read()[0]
 
-        result['context'] = {
-            'default_device_id': self.id,
+        result["context"] = {
+            "default_device_id": self.id,
         }
-        result['domain'] = "[('device_id', '=', " + \
-                           str(self.id) + ")]"
+        result["domain"] = "[('device_id', '=', " + str(self.id) + ")]"
         if len(self.output_ids) == 1:
-            result['views'] = [(False, 'form')]
-            result['res_id'] = self.output_ids.id
+            result["views"] = [(False, "form")]
+            result["res_id"] = self.output_ids.id
         return result
