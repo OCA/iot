9549215edb4c90c1292422817192a1e9aba3432e
Olga Marco <olga.marco@creublanca.es>
2021-06-08 15:27:56 +0200

    [IMP] iot_oca: black, isort, prettier

--- iot_oca/models/iot_device_action.py
+++ iot_oca/models/iot_device_action.py
@@ -1,32 +1,31 @@
 # Copyright (C) 2018 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class IoTDeviceAction(models.Model):
-    _name = 'iot.device.action'
-    _description = 'IoT Action'
-    _order = 'date_ok desc'
+    _name = "iot.device.action"
+    _description = "IoT Action"
+    _order = "date_ok desc"
 
-    device_id = fields.Many2one('iot.device', required=True, readonly=True)
-    system_action_id = fields.Many2one('iot.system.action', required=True)
-    status = fields.Selection([
-        ('ok', 'Ok'),
-        ('pending', 'Pending'),
-        ('failed', 'Failed')
-    ], required=True, default='pending')
+    device_id = fields.Many2one("iot.device", required=True, readonly=True)
+    system_action_id = fields.Many2one("iot.system.action", required=True)
+    status = fields.Selection(
+        [("ok", "Ok"), ("pending", "Pending"), ("failed", "Failed")],
+        required=True,
+        default="pending",
+    )
     result = fields.Text()
     date_ok = fields.Datetime(readonly=True, string="Ok date")
 
     @api.multi
-    @api.constrains('device_id', 'system_action_id')
+    @api.constrains("device_id", "system_action_id")
     def _check_system(self):
         if self.filtered(
             lambda r: r.device_id.system_id != r.system_action_id.system_id
         ):
-            raise ValidationError(_(
-                'Device and action must be of the same system'))
+            raise ValidationError(_("Device and action must be of the same system"))
 
     def run_extra_actions(self, status, result):
         return
@@ -34,11 +33,13 @@ class IoTDeviceAction(models.Model):
     @api.multi
     def run(self):
         self.ensure_one()
-        if self.status != 'ok':
+        if self.status != "ok":
             status, result = self.system_action_id.run(self)
-            self.write({
-                'status': status,
-                'result': result,
-                'date_ok': fields.Datetime.now() if status == 'ok' else False,
-            })
+            self.write(
+                {
+                    "status": status,
+                    "result": result,
+                    "date_ok": fields.Datetime.now() if status == "ok" else False,
+                }
+            )
             self.run_extra_actions(status, result)
