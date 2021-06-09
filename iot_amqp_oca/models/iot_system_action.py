f452eec8806228d21e6b77d1dc017db1b2630fe8
Olga Marco <olga.marco@creublanca.es>
2021-06-09 16:02:30 +0200

    [IMP] iot_amqp_oca: black, isort, prettier

--- iot_amqp_oca/models/iot_system_action.py
+++ iot_amqp_oca/models/iot_system_action.py
@@ -2,9 +2,9 @@ from odoo import models
 
 
 class IoTSystemAction(models.Model):
-    _inherit = 'iot.system.action'
+    _inherit = "iot.system.action"
 
     def _run(self, device_action):
-        if self != self.env.ref('iot_amqp.amqp_action'):
+        if self != self.env.ref("iot_amqp.amqp_action"):
             return super()._run(device_action)
         device_action._run_amqp()
