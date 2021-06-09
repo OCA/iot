5cecdd28801f378f13330b1b6c99c0d91b317e69
Olga Marco <olga.marco@creublanca.es>
2021-06-09 10:03:39 +0200

    [MIG] iot_input_oca: Migration to 13.0

--- iot_input_oca/__manifest__.py
+++ iot_input_oca/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "IoT Input",
-    "version": "12.0.1.3.0",
+    "version": "13.0.1.0.0",
     "category": "IoT",
-    "author": "Creu Blanca, " "Odoo Community Association (OCA)",
+    "author": "Creu Blanca, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "summary": "IoT Input module",
-    "depends": ["iot_output", "mail",],
+    "depends": ["iot_output_oca", "mail"],
     "maintainers": ["etobella"],
     "data": [
         "security/ir.model.access.csv",
