488a8ff138e64a16e03fc434915bbc38a4b92134
Olga Marco <olga.marco@creublanca.es>
2021-06-10 09:44:45 +0200

    [IMP] iot_template_oca: black, isort, prettier

--- iot_template_oca/tests/test_iot_template.py
+++ iot_template_oca/tests/test_iot_template.py
@@ -5,56 +5,57 @@ from odoo.tests.common import TransactionCase
 
 
 class TestIotTemplate(TransactionCase):
-
     def setUp(self):
         super(TestIotTemplate, self).setUp()
-        self.parent_template = self.env['iot.template'].create({
-            'name': 'Parent template',
-            'key_ids': [
-                (0, 0, {
-                    'key': 'passphrase',
-                })
-            ],
-            'input_ids': [
-                (0, 0, {
-                    'name': 'INPUT 1',
-                    'call_model_id': False,
-                    'call_function': 'iot_ras_default_action',
-                    'params': "{'serial': '${serial}', "
-                              "'passphrase': '${passphrase}'}"
-                })
-            ],
-
-        })
-        self.template = self.env['iot.template'].create({
-            'name': 'template',
-            'parent_id': self.parent_template.id,
-            'key_ids': [
-                (0, 0, {
-                    'key': 'serial2',
-                }),
-            ],
-            'input_ids': [
-                (0, 0, {
-                    'name': 'INPUT 2',
-                    'call_model_id': False,
-                    'call_function': 'iot_ras_default_action',
-                    'params': "{'serial': '${serial2}', "
-                              "'passphrase': '${passphrase}'}"
-                })
-            ],
-        })
+        self.parent_template = self.env["iot.template"].create(
+            {
+                "name": "Parent template",
+                "key_ids": [(0, 0, {"key": "passphrase",})],
+                "input_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "INPUT 1",
+                            "call_model_id": False,
+                            "call_function": "iot_ras_default_action",
+                            "params": "{'serial': '${serial}', "
+                            "'passphrase': '${passphrase}'}",
+                        },
+                    )
+                ],
+            }
+        )
+        self.template = self.env["iot.template"].create(
+            {
+                "name": "template",
+                "parent_id": self.parent_template.id,
+                "key_ids": [(0, 0, {"key": "serial2",}),],
+                "input_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "INPUT 2",
+                            "call_model_id": False,
+                            "call_function": "iot_ras_default_action",
+                            "params": "{'serial': '${serial2}', "
+                            "'passphrase': '${passphrase}'}",
+                        },
+                    )
+                ],
+            }
+        )
 
     def test_generation(self):
-        wizard = self.env['iot.device.configure'].create({})
+        wizard = self.env["iot.device.configure"].create({})
         self.assertFalse(wizard.serial)
         wizard.run()
         self.assertTrue(wizard.serial)
-        device_config = self.env['iot.device.configure'].configure(
+        device_config = self.env["iot.device.configure"].configure(
             wizard.serial, self.template.name
         )
-        device = self.env['iot.device'].search([
-            ('name', '=', device_config['name'])])
+        device = self.env["iot.device"].search([("name", "=", device_config["name"])])
         self.assertTrue(device)
         self.assertEqual(1, len(device))
         self.assertEqual(2, len(device.input_ids))
