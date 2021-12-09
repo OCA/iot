633d9a7eddb2bff95973b9264a829af9433cf88c
Enric Tobella <etobella@creublanca.es>
2021-12-09 17:18:10 +0100

    [IMP] iot_amqp_oca: Use configuration

--- iot_amqp_oca/__manifest__.py
+++ iot_amqp_oca/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "IoT AMQP",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "IoT",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -10,6 +10,7 @@
     "summary": "Integrate Iot Outputs with AMQP",
     "depends": ["iot_output_oca"],
     "external_dependencies": {"python": ["pika"]},
+    "website": "https://github.com/OCA/iot",
     "data": [
         "security/ir.model.access.csv",
         "views/iot_amqp_host.xml",
