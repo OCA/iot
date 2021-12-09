c1a6208c839c011ca49b3fc4f5217718c4eb443d
Enric Tobella <etobella@creublanca.es>
2021-12-09 17:58:53 +0100

    [IMP] iot_template_oca: Improve configuration wizard

--- iot_template_oca/__manifest__.py
+++ iot_template_oca/__manifest__.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "IoT Templates",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "IoT",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "summary": "IoT base module",
     "depends": ["iot_input_oca"],
+    "website": "https://github.com/OCA/iot",
     "data": [
         "security/ir.model.access.csv",
         "wizards/iot_device_configure.xml",
