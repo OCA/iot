90e841c0a86cd7c9ca1adfe6483ca313b996dd36
Enric Tobella <etobella@creublanca.es>
2021-12-09 17:58:53 +0100

    [IMP] iot_template_oca: Improve configuration wizard

--- iot_template_oca/wizards/iot_device_configure.py
+++ iot_template_oca/wizards/iot_device_configure.py
@@ -2,42 +2,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from uuid import uuid4
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 
 class IotDeviceConfigure(models.TransientModel):
     _name = "iot.device.configure"
     _description = "Configure a IoT device"
 
-    generated = fields.Boolean(default=False)
-    serial = fields.Char(readonly=True)
+    serial = fields.Char(readonly=True, required=True, default=lambda r: uuid4())
     url = fields.Char(compute="_compute_url")
 
     @api.depends("serial")
     def _compute_url(self):
         for record in self:
-            url = False
-            if record.generated:
-                url = (
-                    self.env["ir.config_parameter"].sudo().get_param("web.base.url")
-                    + "/iot/"
-                    + record.serial
-                    + "/configure"
-                )
-            record.url = url
-
-    def run(self):
-        if not self.generated:
-            self.write({"generated": True, "serial": uuid4()})
-        return {
-            "name": _("Configure device"),
-            "type": "ir.actions.act_window",
-            "res_model": "iot.device.configure",
-            "view_mode": "form",
-            "target": "new",
-            "res_id": self.id,
-            "context": self.env.context,
-        }
+            record.url = (
+                self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+                + "/iot/"
+                + record.serial
+                + "/configure"
+            )
 
     @api.model
     def configure(self, serial, template_id):
