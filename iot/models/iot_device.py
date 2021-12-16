1364ab4684e4343528b08474bd3f37808b6f48c8
Enric Tobella <etobella@creublanca.es>
2021-12-16 11:07:37 +0100

    [IMP] iot_oca: Add Device Image

--- iot/models/iot_device.py
+++ iot/models/iot_device.py
@@ -1,12 +1,30 @@
 # Copyright (C) 2018 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class IoTDevice(models.Model):
-    _name = 'iot.device'
-    _description = 'IoT Device'
-    _order = 'name'
+    _name = "iot.device"
+    _description = "IoT Device"
+    image = fields.Binary(
+        attachment=True,
+        help="This field holds the image used as image for the equipment, "
+             "limited to 1024x1024px.",
+    )
+    image_medium = fields.Binary(
+        string="Medium-sized image",
+        attachment=True,
+        help="Medium-sized image of the equipment. It is automatically "
+             "resized as a 128x128px image, with aspect ratio preserved, "
+             "only when the image exceeds one of those sizes. Use this "
+             "field in form views or some kanban views.")
+    image_small = fields.Binary(
+        string="Small-sized image",
+        attachment=True,
+        help="Small-sized image of the equipment. It is automatically "
+             "resized as a 64x64px image, with aspect ratio preserved. "
+             "Use this field anywhere a small image is required.",
+    )
 
     name = fields.Char(required=True)
     system_id = fields.Many2one('iot.system', required=True)
@@ -44,3 +62,14 @@ class IoTDevice(models.Model):
                 'system_action_id': system_action.id,
             })
             action.run()
+
+    @api.model_create_multi
+    def create(self, values):
+        for vals in values:
+            tools.image_resize_images(vals, sizes={'image': (1024, None)})
+        return super().create(values)
+
+    @api.multi
+    def write(self, vals):
+        tools.image_resize_images(vals, sizes={'image': (1024, None)})
+        return super().write(vals)
