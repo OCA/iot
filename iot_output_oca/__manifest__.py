eac6f46a8775b4b95924d8ec8361af2751e64632
Enric Tobella <etobella@creublanca.es>
2021-12-18 22:32:16 +0100

    [MIG] iot_output_oca: Migration to 14.0

--- iot_output_oca/__manifest__.py
+++ iot_output_oca/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "IoT Output",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
