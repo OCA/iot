aef9c40537d08a0875af9c483116e77addda49e9
Olga Marco <olga.marco@creublanca.es>
2022-05-11 10:14:47 +0200

    [MIG] iot_amqp_oca: Migration to 15.0

--- iot_amqp_oca/__manifest__.py
+++ iot_amqp_oca/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "IoT AMQP",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
     "license": "AGPL-3",
