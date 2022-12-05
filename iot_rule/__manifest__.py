086d5cd181f3c0ba1844bcfbb456fc46c3d86788
AaronHForgeFlow <ahenriquez@eficent.com>
2022-12-05 19:41:17 +0100

    [15.0][MIG] iot_rule

--- iot_rule/__manifest__.py
+++ iot_rule/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "IoT Rule",
     "summary": """
         Define IoT Rules (Keys that control Inputs)""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,ForgeFlow,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/iot",
