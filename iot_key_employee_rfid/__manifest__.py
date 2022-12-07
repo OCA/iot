b3585baf73d37c69ad9279406b831fd827731530
Enric Tobella <etobella@creublanca.es>
2022-12-07 10:19:24 +0100

    [MIG] iot_key_employee_rfid: Migration to 14.0

--- iot_key_employee_rfid/__manifest__.py
+++ iot_key_employee_rfid/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "IoT Key Employee RFID",
     "summary": """
         Use an Employee RFID Card as an IoT Key""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/iot",
     "depends": ["hr_attendance_rfid", "iot_rule"],
     "data": ["views/hr_employee.xml"],
