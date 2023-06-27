532ba18e8c4966801fdca2a5b4f9e65f8f3caa2b
maso <mostafa.barmshory@gmail.com>
2023-06-27 17:08:00 +0330

    [MIG] iot_output_oca: Migration to 16.0

--- iot_output_oca/__manifest__.py
+++ iot_output_oca/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "IoT Output",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
