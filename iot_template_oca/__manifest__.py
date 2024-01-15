1ae2319f26858cb66911eb70f42e37226109ee77
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2024-01-15 17:35:54 +0100

    [MIG] iot_template_oca: migration to v16

--- iot_template_oca/__manifest__.py
+++ iot_template_oca/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "IoT Templates",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
     "license": "AGPL-3",
