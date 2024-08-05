b4cf2ddccd9b876dd084040b63f38c5375b6fa2f
FernandoRomera <fromera@astrohotel.es>
2024-08-05 10:16:46 +0200

    [IMP] iot_output_oca: pre-commit auto fixes

--- iot_output_oca/tests/test_iot.py
+++ iot_output_oca/tests/test_iot.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2018 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from mock import patch
+from unittest.mock import patch
 
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
